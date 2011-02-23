4cd758d9f1de4342b3ae5d5fcd37a17859abe9f8
TAKATSU Tomonari <tota@rtfm.jp>
2011-02-23 21:39:46 +0900

    - Fix bug that ENV['PATH'] swells whenever make_image_by_mimeTeX function   is called, advised by @ktou

--- files/patch-tex.rb
+++ files/patch-tex.rb
@@ -1,10 +1,11 @@
 --- lib/rabbit/parser/ext/tex.rb.orig	2010-12-31 18:25:18.000000000 +0900
-+++ lib/rabbit/parser/ext/tex.rb	2011-02-23 00:32:23.000000000 +0900
-@@ -45,6 +45,7 @@
++++ lib/rabbit/parser/ext/tex.rb	2011-02-23 21:35:40.000000000 +0900
+@@ -44,7 +44,7 @@
+         def make_image_by_mimeTeX(path, prop, logger)
            image_file = Tempfile.new("rabbit-image-mimetex")
            args = ["-e", image_file.path, "-f", path]
-           commands = ["mimetex", "mimetex.cgi"]
-+          ENV.store('PATH', ENV['PATH'] + ":%%LOCALBASE%%/www/mimetex/cgi-bin")
+-          commands = ["mimetex", "mimetex.cgi"]
++          commands = ["mimetex", "mimetex.cgi", "%%LOCALBASE%%/www/mimetex/cgi-bin/mimetex.cgi"]
            if commands.any? {|command| SystemRunner.run(command, *args)}
              image_file
            else
